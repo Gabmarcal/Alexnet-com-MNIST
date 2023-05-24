ce084bd25013fd1876e75839d2238acadfba016b
Gabmarcal <gabrielmarcalmendonca@gmail.com>
2023-05-24 00:28:16 -0300

    Dados com CPU

--- alexnet_sem_treino.py
+++ alexnet_sem_treino.py
@@ -8,7 +8,7 @@ import time
 import pandas as pd
 
 # Verificar se a GPU está disponível
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu") #"cuda" if torch.cuda.is_available() else "cpu"
 
 print('Iniciando Código')
 
